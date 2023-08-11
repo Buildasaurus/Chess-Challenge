f64a7eebfbe56ea777c72e7755f0d9084b4a40a6
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-08-11 14:26:40 +0200

    Updated EvilBot to use iterative deepening

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -8,8 +8,7 @@ using System;
 
 namespace ChessChallenge.Example
 {
-	// A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-	// Plays randomly otherwise.
+
 	public class EvilBot : IChessBot
 	{
 		List<int> counters = new List<int>();
@@ -43,11 +42,14 @@ namespace ChessChallenge.Example
 				return -5;
 			return kingMatrix[y, x];
 		}
-		int depth = 3;
-
-		public Move Think(Board board, Timer timer)
+		bool timeToStop = false;
+		Timer timer;
+		/// <summary>
+		/// if under this miliseconds remaing, then should stop.
+		/// </summary>
+		int endMiliseconds = 0;
+		public Move Think(Board board, Timer _timer)
 		{
-			Move[] moves = board.GetLegalMoves();
 			/* Can't handle the time - it time outs. even if on so low as 8000 moves, adding two more depth takes it to 800.000
 			if(counters.Count > 3)
 			{
@@ -57,11 +59,96 @@ namespace ChessChallenge.Example
 				if (sum > 500000 && depth > 3)
 					depth -= 2;
 			}*/
-			return bestMove(board, depth, board.IsWhiteToMove);
+			timer = _timer;
+			endMiliseconds = (int)Math.Ceiling(timer.MillisecondsRemaining * 0.98f);
+			timeToStop = false;
+			return bestMove(board, board.IsWhiteToMove);
+		}
+		int[] pieceValues = { 100, 300, 300, 500, 900, 99999 };
+		Move bestMove(Board board, bool playerToMove)
+		{
+			counters.Add(0);
+			evalCounter = 0;
+			Move[] legalmoves = board.GetLegalMoves();
+
+			Move bestmove = legalmoves[0];
+			int color = playerToMove ? 1 : -1;
+			float bestEval = -1000000;
+			Array.Sort(legalmoves, (a, b) => MoveOrderingHeuristic(b, board).CompareTo(MoveOrderingHeuristic(a, board)));
+			for (int d = 1; d <= 9; d++)
+			{
+				bestEval = -1000000; //must reset besteval, or it will not reach as good evals
+									 //as last round, and not realize there are better moves
+
+				Console.WriteLine($"bestmove was {bestmove}, now going at depth {d}");
+
+				int index = Array.IndexOf(legalmoves, bestmove);
+				if (index > 0)
+				{
+					for (int i = index; i > 0; i--)
+					{
+						legalmoves[i] = legalmoves[i - 1];
+					}
+					legalmoves[0] = bestmove;
+				}
+
+				foreach (Move move in legalmoves)
+				{
+					if (timeToStop && d > 3)
+					{
+						break;
+					}
+					board.MakeMove(move);
+					float eval = -negamax(board, d, -1000000, 1000000, -color);
+					if (eval > bestEval)
+					{
+						bestEval = eval;
+						bestmove = move;
+					}
+					board.UndoMove(move);
+					//Console.WriteLine($"Move {move} was {eval}");
+
+				}
+				Console.WriteLine($"Final best move was {bestmove} with eval at {bestEval}");
+				if (timeToStop && d >= 3)
+				{
+					break;
+				}
+			}
+			Console.WriteLine("mybot " + counters.Count);
+			Console.WriteLine("mybit " + evalCounter);
+			return bestmove;
 		}
 
-		int[] pieceValues = { 100, 300, 300, 500, 900, 99999 };
 
+
+		int MoveOrderingHeuristic(Move move, Board board)
+		{
+			int score = 0;
+
+			// Give higher scores to captures and promotions
+			if (move.IsCapture)
+			{
+				// Use MVV-LVA heuristic
+				score += 10 * pieceValues[(int)move.CapturePieceType - 1] - pieceValues[(int)move.MovePieceType - 1];
+				if (!board.SquareIsAttackedByOpponent(move.TargetSquare))
+					score = 100000;
+			}
+			if (move.IsPromotion)
+				score += 900;
+			//give small bonus for perhaps retaking or taking the last piece moved.
+			if (board.GameMoveHistory.Length > 0 && board.GameMoveHistory[^1].TargetSquare == move.TargetSquare)
+				score += 50;
+			// Give a small bonus for checks
+			board.MakeMove(move);
+			if (board.IsInCheck())
+				score += 100;
+			board.UndoMove(move);
+			if (board.SquareIsAttackedByOpponent(move.TargetSquare))
+				score -= pieceValues[(int)move.MovePieceType - 1];
+
+			return score;
+		}
 
 		float evaluation(Board board)
 		{
@@ -70,10 +157,85 @@ namespace ChessChallenge.Example
 			eval += materialEval(board, true);
 			eval -= materialEval(board, false);
 
+			// Initialize variables for pawn structure and king safety evaluation
+			/*ulong whitePawns = board.GetPieceBitboard(PieceType.Pawn, true);
+			ulong blackPawns = board.GetPieceBitboard(PieceType.Pawn, false);
+			ulong whiteIsolatedPawns = ~((whitePawns << 1) | (whitePawns >> 1));
+			ulong blackIsolatedPawns = ~((blackPawns << 1) | (blackPawns >> 1));
+			ulong whitePieces = board.WhitePiecesBitboard;
+			ulong blackPieces = board.BlackPiecesBitboard;
+			Square whiteKingSquare = board.GetKingSquare(true);
+			Square blackKingSquare = board.GetKingSquare(false);
+
+			// Evaluate doubled pawns
+			for (int file = 0; file < 8; file++)
+			{
+				int whitePawnsOnFile = BitOperations.PopCount(whitePawns & FileMask(file));
+				int blackPawnsOnFile = BitOperations.PopCount(blackPawns & FileMask(file));
+				if (whitePawnsOnFile > 1)
+					eval -= whitePawnsOnFile - 1;
+				if (blackPawnsOnFile > 1)
+					eval += blackPawnsOnFile - 1;
+			}
+
+			// Evaluate isolated pawns
+			eval -= BitOperations.PopCount(whiteIsolatedPawns & whitePawns);
+			eval += BitOperations.PopCount(blackIsolatedPawns & blackPawns);
+
+			// Evaluate king safety
+			int whiteKingFile = whiteKingSquare.File;
+			int whiteKingRank = whiteKingSquare.Rank;
+
+			// Check for open files towards the king
+			ulong fileMask = FileMask(whiteKingFile);
+			if ((fileMask & ~(RankMask(whiteKingRank) - 1) & ~(whitePieces | blackPieces)) == (fileMask & ~(RankMask(whiteKingRank) - 1)))
+				eval -= 0.5f;
+
+			// Check for open diagonals towards the king
+			ulong diagonalMask = DiagonalMask(whiteKingSquare);
+			if ((diagonalMask & ~(RankMask(whiteKingRank) - 1) & ~(whitePieces | blackPieces)) == (diagonalMask & ~(RankMask(whiteKingRank) - 1)))
+				eval -= 0.5f;
+
+			// Evaluate king safety for black
+			int blackKingFile = blackKingSquare.File;
+			int blackKingRank = blackKingSquare.Rank;
+
+			// Check for open files towards the king
+			fileMask = FileMask(blackKingFile);
+			if ((fileMask & (RankMask(blackKingRank + 1) - 1) & ~(whitePieces | blackPieces)) == (fileMask & (RankMask(blackKingRank + 1) - 1)))
+				eval += 0.5f;
+
+			// Check for open diagonals towards the king
+			diagonalMask = DiagonalMask(blackKingSquare);
+			if ((diagonalMask & (RankMask(blackKingRank + 1) - 1) & ~(whitePieces | blackPieces)) == (diagonalMask & (RankMask(blackKingRank + 1) - 1)))
+				eval += 0.5f;
+			*/
 			return eval;
 		}
+		/*
+		ulong FileMask(int file)
+		{
+			return 0x0101010101010101UL << file;
+		}
+		ulong RankMask(int rank)
+		{
+			return 0xffUL << (rank * 8);
+		}
+		ulong DiagonalMask(Square square)
+		{
+			int rank = square.Rank;
+			int file = square.File;
+			ulong mask = 0;
+			for (int i = 1; i < Math.Min(rank, file) + 1; i++)
+				mask |= 1UL << ((rank - i) * 8 + file - i);
+			for (int i = 1; i < Math.Min(7 - rank, file) + 1; i++)
+				mask |= 1UL << ((rank + i) * 8 + file - i);
+			return mask;
+		}*/
+
 		int materialEval(Board board, bool color)
 		{
+			if (!timeToStop && timer.MillisecondsRemaining < endMiliseconds) timeToStop = true;
 			int eval = 0;
 			PieceType[] piectypes = { PieceType.Pawn, PieceType.Knight, PieceType.Bishop, PieceType.Rook,
 			PieceType.Queen, PieceType.King};
@@ -101,58 +263,12 @@ namespace ChessChallenge.Example
 					}
 				}
 			}
+
 			return eval;
 		}
 
 
-		Move bestMove(Board board, int depth, bool playerToMove)
-		{
-			counters.Add(0);
-			evalCounter = 0;
-			Move bestmove = board.GetLegalMoves()[0];
-			int color = playerToMove ? 1 : -1;
-			float bestEval = -1000000;
-			Move[] legalmoves = board.GetLegalMoves();
-			Array.Sort(legalmoves, (a, b) => MoveOrderingHeuristic(b, board).CompareTo(MoveOrderingHeuristic(a, board)));
 
-			foreach (Move move in legalmoves)
-			{
-				board.MakeMove(move);
-				float eval = -negamax(board, depth, -1000000, 1000000, -color);
-				if (eval > bestEval)
-				{
-					bestEval = eval;
-					bestmove = move;
-				}
-				board.UndoMove(move);
-			}
-
-			Console.WriteLine(counters[^1]);
-			Console.WriteLine(evalCounter);
-			return bestmove;
-		}
-
-		int MoveOrderingHeuristic(Move move, Board board)
-		{
-			int score = 0;
-
-			// Give higher scores to captures and promotions
-			if (move.IsCapture)
-			{
-				// Use MVV-LVA heuristic
-				score += 1000 * (pieceValues[(int)move.CapturePieceType - 1] - pieceValues[(int)move.MovePieceType - 1]);
-			}
-			if (move.IsPromotion)
-				score += 500;
-
-			// Give a small bonus for checks
-			board.MakeMove(move);
-			if (board.IsInCheck())
-				score += 10;
-			board.UndoMove(move);
-
-			return score;
-		}
 
 		/*
 		int StaticExchangeEvaluation(Move move, Board board)
