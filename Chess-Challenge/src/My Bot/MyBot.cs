f582974882a057c85f00307bf5fd3fd377515999
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-08-27 14:29:45 +0200

    Less tokens same bot
    Score of MyBot vs EvilBot: 189 - 187 - 164  [0.502] 540
    ...      MyBot playing White: 112 - 81 - 78  [0.557] 271
    ...      MyBot playing Black: 77 - 106 - 86  [0.446] 269
    ...      White vs Black: 218 - 158 - 164  [0.556] 540
    Elo difference: 1.3 +/- 24.5, LOS: 54.1 %, DrawRatio: 30.4 %

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -26,7 +26,6 @@ public class MyBot : IChessBot
 		73949978050619586491881614568m, 77043619187199676893167803647m, 1212557245150259869494540530m, 3081561358716686153294085872m, 3392217589357453836837847030m, 1219782446916489227407330320m, 78580145051212187267589731866m, 75798434925965430405537592305m,
 		68369566912511282590874449920m, 72396532057599326246617936384m, 75186737388538008131054524416m, 77027917484951889231108827392m, 73655004947793353634062267392m, 76417372019396591550492896512m, 74568981255592060493492515584m, 70529879645288096380279255040m,
 	};
-	int[][] UnpackedPestoTables;
 
 
 
@@ -50,7 +49,6 @@ public class MyBot : IChessBot
 		}
 	}*/
 	// History table definition
-	int[,,] historyTable;
 	// Define a structure to store transposition table entries
 	int lookups = 0; //#DEBUG
 	int entryCount = 0;//#DEBUG
@@ -63,16 +61,12 @@ public class MyBot : IChessBot
 	// Create a transposition table // key, move, score/eval, depth, flag.
 	private readonly (ulong, Move, int, sbyte, byte)[] transpositionTable = new (ulong, Move, int, sbyte, byte)[0x400000];
 
-	bool timeToStop;
 
 	Move overAllBestMove;
-	Timer timer;
-	int timeForTurn;
-	public Move Think(Board _board, Timer _timer)
+	public Move Think(Board board, Timer timer)
 	{
-		board = _board;
-		if (board.GameMoveHistory.Length < 2)
-			UnpackedPestoTables = PackedPestoTables.Select(packedTable =>
+		//Saves tokens to unpack every time.
+		int[][] UnpackedPestoTables = PackedPestoTables.Select(packedTable =>
 			{
 				int pieceType = 0;
 				return decimal.GetBits(packedTable).Take(3)
@@ -82,11 +76,205 @@ public class MyBot : IChessBot
 			}).ToArray();
 
 		Console.WriteLine("-----NBEW bot thinking----");//#DEBUG
-														//killerMoves.Clear();
-		timer = _timer;
-		historyTable = new int[2, 7, 64];
-		timeForTurn = Math.Min(timer.MillisecondsRemaining - 50, timer.MillisecondsRemaining / 30);
-		timeToStop = false;
+
+		int[,,] historyTable = new int[2, 7, 64];
+		int timeForTurn = Math.Min(timer.MillisecondsRemaining - 50, timer.MillisecondsRemaining / 30);
+		bool timeToStop = false;
+		/// <summary>
+		/// Assumes global board variable exists, and evaluates that
+		/// </summary>
+		/// <returns></returns>
+		int evaluation()
+		{
+			int middlegame = 0, endgame = 0, gamephase = 0, sideToMove = 2;
+			for (; --sideToMove >= 0;)
+			{
+				for (int piece = -1, square; ++piece < 6;)
+					for (ulong mask = board.GetPieceBitboard((PieceType)piece + 1, sideToMove > 0); mask != 0;)
+					{
+						//gamephase goes from 0 to 24, 24 is midgame, 0 is endgame
+						gamephase += phase_weight[piece];
+						//Get index of first bit, which is index of the piece- Then XOR if it's white, to flip the board,
+						//cuz tables are opposite, so 0,0 isn't a1, but a8.
+						square = BitboardHelper.ClearAndGetIndexOfLSB(ref mask) ^ 56 * sideToMove;
+						middlegame += UnpackedPestoTables[square][piece];
+						endgame += UnpackedPestoTables[square][piece + 6];
+					}
+
+				middlegame *= -1;
+				endgame *= -1;
+			}
+			return (middlegame * gamephase + endgame * (24 - gamephase)) / 24;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="board"></param>
+		/// <param name="depth">Depth left to search - falls</param>
+		/// <param name="ply">How deep you have made it - rises</param>
+		/// <param name="alpha"></param>
+		/// <param name="beta"></param>
+		/// <param name="color"></param>
+		/// <param name="numExtensions"></param>
+		/// <returns></returns>
+		int negamax(sbyte depth, int ply, int alpha, int beta)
+		{
+			depth = Math.Max(depth, (sbyte)0);
+			//start searching
+
+			int oldAlpha = alpha, R = 2, movesScored = 0, moveCount = 0, max = -100000000;
+
+			if (depth < 0) Console.WriteLine("smaller than 0"); //#DEBUG
+																//Much used variables
+			bool isPV = beta - alpha > 1, notRoot = ply > 0, isInCheck = board.IsInCheck();
+
+			//Draw detection
+			if (notRoot && board.IsDraw())
+				return 0; //slight discouragement of draws.
+			if (board.IsInCheckmate())
+				return ply - 999999;
+
+			//Debug
+			counters[^1]++; //#DEBUG
+
+
+
+			//check extensions - MUST BE BEFORE QSEARCH	
+			if (isInCheck)
+				depth++;
+
+			//QSearch
+			bool isQSearch = depth <= 0;
+			if (isQSearch)
+			{
+				max = evaluation() * (board.IsWhiteToMove ? 1 : -1);
+
+				if (max >= beta)
+					return max;
+				alpha = Math.Max(alpha, max);
+			}
+			//local search function to save tokens.
+			int search(int reductions, int betas) => -negamax((sbyte)(depth - 1 - reductions), ply + 1, -betas, -alpha);
+
+			// Null move pruning - is perhaps best with more time on the clock?
+			if (!isPV && !isInCheck && depth > R)
+			{
+				board.TrySkipTurn();
+				int score = search(R, beta);
+				board.UndoSkipTurn();
+				if (score >= beta)
+					return score;
+			}
+			// Transposition table lookup
+			ulong zobristHash = board.ZobristKey;
+			ref var entry = ref transpositionTable[zobristHash & 0x3FFFFF];
+			int entryScore = entry.Item3, entryFlag = entry.Item5;
+
+			//If we have an "exact" score (a < score < beta) just use that
+			//If we have a lower bound better than beta, use that
+			//If we have an upper bound worse than alpha, use that
+			if (notRoot && entry.Item1 == zobristHash && entry.Item4 >= depth && Math.Abs(entryScore) < 50000 && (
+					// Exact
+					entryFlag == 1 ||
+					// Upperbound
+					entryFlag == 2 && entryScore <= alpha ||
+					// Lowerbound
+					entryFlag == 3 && entryScore >= beta))
+			{
+				lookups++; //#DEBUG
+				return entryScore;
+			}
+
+
+			if (!notRoot) Console.WriteLine($"info string Bestmove at depth{depth} was for a starter: {overAllBestMove}");//#DEBUG
+
+			// Generate legal moves and sort them
+			Move goodMove = notRoot ? entry.Item2 : overAllBestMove, bestFoundMove = Move.NullMove;
+
+			// Gamestate, checkmate and draws
+			Span<Move> legalmoves = stackalloc Move[218];
+			board.GetLegalMovesNonAlloc(ref legalmoves, isQSearch);
+
+			foreach (Move move in legalmoves)
+				moveScores[movesScored++] = -(
+				// Hash move
+				move == goodMove ? 9_000_000 :
+				// MVVLVA
+				move.IsCapture ? 1_000_000 * (int)move.CapturePieceType - (int)move.MovePieceType :
+				// History
+				historyTable[board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType, move.TargetSquare.Index]);
+
+			moveScores.AsSpan(0, legalmoves.Length).Sort(legalmoves);
+
+			// if we are at root level, make sure that the overallbest move from earlier iterations is at top.
+			foreach (Move move in legalmoves)
+			{
+				int eval;
+				moveCount++; // Increment the move counter
+
+				//Early stop at top level
+				if (!timeToStop && timer.MillisecondsElapsedThisTurn > timeForTurn) timeToStop = true;
+				if (timeToStop)
+					return 0;
+
+				board.MakeMove(move);
+				if (isQSearch)
+				{
+					eval = search(depth, beta);
+				}
+				else
+				{
+					// LMR: reduce the depth of the search for moves beyond a certain move count threshold - Can save few tokens here with simpler reduction
+					int reduction = (int)((depth >= 4 && moveCount >= 4 && !move.IsCapture && !move.IsPromotion && !isInCheck && !isPV) ? 1 + Math.Log2(depth) * Math.Log2(moveCount) / 2 : 0);
+					//reduction = isPV && reduction > 0 ? 1 : 0;
+
+					if (moveCount == 1)
+						eval = search(reduction, beta);
+					else
+					{
+						eval = search(reduction, alpha + 1); //should be +, because it will be negated in the search method.
+						if (eval > alpha || (reduction > 0 && beta > eval))
+							eval = search(0, beta);
+					}
+
+
+				}
+				if (eval > max)
+				{
+					//if root level new best move is found, then save it to be played or for next iteration 
+					if (ply == 0 && !timeToStop)
+					{
+						overAllBestMove = move;
+						Console.WriteLine($"info string new Overall Best move: {move}");//#DEBUG
+					}
+					bestFoundMove = move;
+					max = eval;
+				}
+				board.UndoMove(move);
+
+				alpha = Math.Max(alpha, max);
+				if (alpha >= beta)
+				{
+					//if move causes beta-cutoff, it's nice, so it's "score" is now increased, depending on how early it did the beta-cutoff. yes?
+					historyTable[board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType, move.TargetSquare.Index] += depth * depth;
+					break;
+				}
+			}
+
+
+			// Transposition table store
+			entryCount++; //#DEBUG
+						  // Transposition table insertion
+			entry = new(
+				zobristHash,
+				bestFoundMove == default ? entry.Item2 : bestFoundMove,
+				max,
+				depth,
+				(byte)(max >= beta ? 3 : max <= oldAlpha ? 2 : 1));
+
+			return max;
+		}
 		lookups = 0; //#DEBUG
 		entryCount = 0; //#DEBUG
 		counters.Add(0);//#DEBUG
@@ -117,201 +305,5 @@ public class MyBot : IChessBot
 
 
 
-	/// <summary>
-	/// Assumes global board variable exists, and evaluates that
-	/// </summary>
-	/// <returns></returns>
-	int evaluation()
-	{
-		int middlegame = 0, endgame = 0, gamephase = 0, sideToMove = 2;
-		for (; --sideToMove >= 0;)
-		{
-			for (int piece = -1, square; ++piece < 6;)
-				for (ulong mask = board.GetPieceBitboard((PieceType)piece + 1, sideToMove > 0); mask != 0;)
-				{
-					//gamephase goes from 0 to 24, 24 is midgame, 0 is endgame
-					gamephase += phase_weight[piece];
-					//Get index of first bit, which is index of the piece- Then XOR if it's white, to flip the board,
-					//cuz tables are opposite, so 0,0 isn't a1, but a8.
-					square = BitboardHelper.ClearAndGetIndexOfLSB(ref mask) ^ 56 * sideToMove;
-					middlegame += UnpackedPestoTables[square][piece];
-					endgame += UnpackedPestoTables[square][piece + 6];
-				}
-
-			middlegame *= -1;
-			endgame *= -1;
-		}
-		return (middlegame * gamephase + endgame * (24 - gamephase)) / 24;
-	}
-
-	//Global board saves tokens.
-	Board board;
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="board"></param>
-	/// <param name="depth">Depth left to search - falls</param>
-	/// <param name="ply">How deep you have made it - rises</param>
-	/// <param name="alpha"></param>
-	/// <param name="beta"></param>
-	/// <param name="color"></param>
-	/// <param name="numExtensions"></param>
-	/// <returns></returns>
-	int negamax(sbyte depth, int ply, int alpha, int beta)
-	{
-		depth = Math.Max(depth, (sbyte)0);
-		//start searching
-
-		int oldAlpha = alpha, R = 2, movesScored = 0, moveCount = 0, max = -100000000;
-
-		if (depth < 0) Console.WriteLine("smaller than 0"); //#DEBUG
-															//Much used variables
-		bool isPV = beta - alpha > 1, notRoot = ply > 0, isInCheck = board.IsInCheck();
-
-		//Draw detection
-		if (notRoot && board.IsDraw())
-			return 0; //slight discouragement of draws.
-		if (board.IsInCheckmate())
-			return ply - 999999;
-
-		//Debug
-		counters[^1]++; //#DEBUG
-
-
-
-		//check extensions - MUST BE BEFORE QSEARCH	
-		if (isInCheck)
-			depth++;
-
-		//QSearch
-		bool isQSearch = depth <= 0;
-		if (isQSearch)
-		{
-			max = evaluation() * (board.IsWhiteToMove ? 1 : -1);
-
-			if (max >= beta)
-				return max;
-			alpha = Math.Max(alpha, max);
-		}
-		//local search function to save tokens.
-		int search(int reductions, int betas) => -negamax((sbyte)(depth - 1 - reductions), ply + 1, -betas, -alpha);
-
-		// Null move pruning - is perhaps best with more time on the clock?
-		if (!isPV && !isInCheck && depth > R)
-		{
-			board.TrySkipTurn();
-			int score = search(R, beta);
-			board.UndoSkipTurn();
-			if (score >= beta)
-				return score;
-		}
-		// Transposition table lookup
-		ulong zobristHash = board.ZobristKey;
-		ref var entry = ref transpositionTable[zobristHash & 0x3FFFFF];
-		int entryScore = entry.Item3, entryFlag = entry.Item5;
-
-		//If we have an "exact" score (a < score < beta) just use that
-		//If we have a lower bound better than beta, use that
-		//If we have an upper bound worse than alpha, use that
-		if (notRoot && entry.Item1 == zobristHash && entry.Item4 >= depth && Math.Abs(entryScore) < 50000 && (
-				// Exact
-				entryFlag == 1 ||
-				// Upperbound
-				entryFlag == 2 && entryScore <= alpha ||
-				// Lowerbound
-				entryFlag == 3 && entryScore >= beta))
-		{
-			lookups++; //#DEBUG
-			return entryScore;
-		}
-
-
-		if (!notRoot) Console.WriteLine($"info string Bestmove at depth{depth} was for a starter: {overAllBestMove}");//#DEBUG
-
-		// Generate legal moves and sort them
-		Move goodMove = notRoot ? entry.Item2 : overAllBestMove, bestFoundMove = Move.NullMove;
-
-		// Gamestate, checkmate and draws
-		Span<Move> legalmoves = stackalloc Move[218];
-		board.GetLegalMovesNonAlloc(ref legalmoves, isQSearch);
-
-		foreach (Move move in legalmoves)
-			moveScores[movesScored++] = -(
-			// Hash move
-			move == goodMove ? 9_000_000 :
-			// MVVLVA
-			move.IsCapture ? 1_000_000 * (int)move.CapturePieceType - (int)move.MovePieceType :
-			// History
-			historyTable[board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType, move.TargetSquare.Index]);
-
-		moveScores.AsSpan(0, legalmoves.Length).Sort(legalmoves);
-
-		// if we are at root level, make sure that the overallbest move from earlier iterations is at top.
-		foreach (Move move in legalmoves)
-		{
-			int eval;
-			moveCount++; // Increment the move counter
-
-			//Early stop at top level
-			if (!timeToStop && timer.MillisecondsElapsedThisTurn > timeForTurn) timeToStop = true;
-			if (timeToStop)
-				return 0;
-
-			board.MakeMove(move);
-			if (isQSearch)
-			{
-				eval = search(depth, beta);
-			}
-			else
-			{
-				// LMR: reduce the depth of the search for moves beyond a certain move count threshold - Can save few tokens here with simpler reduction
-				int reduction = (int)((depth >= 4 && moveCount >= 4 && !move.IsCapture && !move.IsPromotion && !isInCheck && !isPV) ? 1 + Math.Log2(depth) * Math.Log2(moveCount) / 2 : 0);
-				//reduction = isPV && reduction > 0 ? 1 : 0;
-
-				if (moveCount == 1)
-					eval = search(reduction, beta);
-				else
-				{
-					eval = search(reduction, alpha + 1); //should be +, because it will be negated in the search method.
-					if (eval > alpha || (reduction > 0 && beta > eval))
-						eval = search(0, beta);
-				}
-
-
-			}
-			if (eval > max)
-			{
-				//if root level new best move is found, then save it to be played or for next iteration 
-				if (ply == 0 && !timeToStop)
-				{
-					overAllBestMove = move;
-					Console.WriteLine($"info string new Overall Best move: {move}");//#DEBUG
-				}
-				bestFoundMove = move;
-				max = eval;
-			}
-			board.UndoMove(move);
-
-			alpha = Math.Max(alpha, max);
-			if (alpha >= beta)
-			{
-				//if move causes beta-cutoff, it's nice, so it's "score" is now increased, depending on how early it did the beta-cutoff. yes?
-				historyTable[board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType, move.TargetSquare.Index] += depth * depth;
-				break;
-			}
-		}
-
-
-		// Transposition table store
-		entryCount++; //#DEBUG
-						// Transposition table insertion
-		entry = new(
-			zobristHash,
-			bestFoundMove == default ? entry.Item2 : bestFoundMove,
-			max,
-			depth,
-			(byte)(max >= beta ? 3 : max <= oldAlpha ? 2 : 1));
-
-		return max;
-	}
+	
 }
