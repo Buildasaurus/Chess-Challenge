5cf8e60ff9390d2d49c887f252da91f331e7ef5b
Buildasaurus <jonathan.dyrekilde.sommerlund@gmail.com>
2023-08-27 13:30:31 +0200

    less tokens
    Seems same
    
    Score of MyBot vs EvilBot: 481 - 505 - 452  [0.492] 1438
    ...      MyBot playing White: 306 - 183 - 231  [0.585] 720
    ...      MyBot playing Black: 175 - 322 - 221  [0.398] 718
    ...      White vs Black: 628 - 358 - 452  [0.594] 1438
    Elo difference: -5.8 +/- 14.9, LOS: 22.2 %, DrawRatio: 31.4 %

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -17,15 +17,15 @@ public class MyBot : IChessBot
 	List<int> counters = new List<int>(); //#DEBUG
 
 	private readonly decimal[] PackedPestoTables = {
-			63746705523041458768562654720m, 71818693703096985528394040064m, 75532537544690978830456252672m, 75536154932036771593352371712m, 76774085526445040292133284352m, 3110608541636285947269332480m, 936945638387574698250991104m, 75531285965747665584902616832m,
-			77047302762000299964198997571m, 3730792265775293618620982364m, 3121489077029470166123295018m, 3747712412930601838683035969m, 3763381335243474116535455791m, 8067176012614548496052660822m, 4977175895537975520060507415m, 2475894077091727551177487608m,
-			2458978764687427073924784380m, 3718684080556872886692423941m, 4959037324412353051075877138m, 3135972447545098299460234261m, 4371494653131335197311645996m, 9624249097030609585804826662m, 9301461106541282841985626641m, 2793818196182115168911564530m,
-			77683174186957799541255830262m, 4660418590176711545920359433m, 4971145620211324499469864196m, 5608211711321183125202150414m, 5617883191736004891949734160m, 7150801075091790966455611144m, 5619082524459738931006868492m, 649197923531967450704711664m,
-			75809334407291469990832437230m, 78322691297526401047122740223m, 4348529951871323093202439165m, 4990460191572192980035045640m, 5597312470813537077508379404m, 4980755617409140165251173636m, 1890741055734852330174483975m, 76772801025035254361275759599m,
-			75502243563200070682362835182m, 78896921543467230670583692029m, 2489164206166677455700101373m, 4338830174078735659125311481m, 4960199192571758553533648130m, 3420013420025511569771334658m, 1557077491473974933188251927m, 77376040767919248347203368440m,
-			73949978050619586491881614568m, 77043619187199676893167803647m, 1212557245150259869494540530m, 3081561358716686153294085872m, 3392217589357453836837847030m, 1219782446916489227407330320m, 78580145051212187267589731866m, 75798434925965430405537592305m,
-			68369566912511282590874449920m, 72396532057599326246617936384m, 75186737388538008131054524416m, 77027917484951889231108827392m, 73655004947793353634062267392m, 76417372019396591550492896512m, 74568981255592060493492515584m, 70529879645288096380279255040m,
-		};
+		63746705523041458768562654720m, 71818693703096985528394040064m, 75532537544690978830456252672m, 75536154932036771593352371712m, 76774085526445040292133284352m, 3110608541636285947269332480m, 936945638387574698250991104m, 75531285965747665584902616832m,
+		77047302762000299964198997571m, 3730792265775293618620982364m, 3121489077029470166123295018m, 3747712412930601838683035969m, 3763381335243474116535455791m, 8067176012614548496052660822m, 4977175895537975520060507415m, 2475894077091727551177487608m,
+		2458978764687427073924784380m, 3718684080556872886692423941m, 4959037324412353051075877138m, 3135972447545098299460234261m, 4371494653131335197311645996m, 9624249097030609585804826662m, 9301461106541282841985626641m, 2793818196182115168911564530m,
+		77683174186957799541255830262m, 4660418590176711545920359433m, 4971145620211324499469864196m, 5608211711321183125202150414m, 5617883191736004891949734160m, 7150801075091790966455611144m, 5619082524459738931006868492m, 649197923531967450704711664m,
+		75809334407291469990832437230m, 78322691297526401047122740223m, 4348529951871323093202439165m, 4990460191572192980035045640m, 5597312470813537077508379404m, 4980755617409140165251173636m, 1890741055734852330174483975m, 76772801025035254361275759599m,
+		75502243563200070682362835182m, 78896921543467230670583692029m, 2489164206166677455700101373m, 4338830174078735659125311481m, 4960199192571758553533648130m, 3420013420025511569771334658m, 1557077491473974933188251927m, 77376040767919248347203368440m,
+		73949978050619586491881614568m, 77043619187199676893167803647m, 1212557245150259869494540530m, 3081561358716686153294085872m, 3392217589357453836837847030m, 1219782446916489227407330320m, 78580145051212187267589731866m, 75798434925965430405537592305m,
+		68369566912511282590874449920m, 72396532057599326246617936384m, 75186737388538008131054524416m, 77027917484951889231108827392m, 73655004947793353634062267392m, 76417372019396591550492896512m, 74568981255592060493492515584m, 70529879645288096380279255040m,
+	};
 	int[][] UnpackedPestoTables;
 
 
@@ -58,7 +58,7 @@ public class MyBot : IChessBot
 
 	private readonly int[] moveScores = new int[218], phase_weight = { 0, 1, 1, 2, 4, 0 },
 				PieceValues = { 82, 337, 365, 477, 1025, 0, // Middlegame
-									94, 281, 297, 512, 936, 0}; // Endgame
+								94, 281, 297, 512, 936, 0}; // Endgame
 
 	// Create a transposition table // key, move, score/eval, depth, flag.
 	private readonly (ulong, Move, int, sbyte, byte)[] transpositionTable = new (ulong, Move, int, sbyte, byte)[0x400000];
@@ -66,10 +66,11 @@ public class MyBot : IChessBot
 	bool timeToStop;
 
 	Move overAllBestMove;
-	ChessChallenge.API.Timer timer;
+	Timer timer;
 	int timeForTurn;
-	public Move Think(Board board, ChessChallenge.API.Timer _timer)
+	public Move Think(Board _board, Timer _timer)
 	{
+		board = _board;
 		if (board.GameMoveHistory.Length < 2)
 			UnpackedPestoTables = PackedPestoTables.Select(packedTable =>
 			{
@@ -80,8 +81,8 @@ public class MyBot : IChessBot
 					.ToArray();
 			}).ToArray();
 
-		Console.WriteLine("-----My bot thinking----");//#DEBUG
-													  //killerMoves.Clear();
+		Console.WriteLine("-----NBEW bot thinking----");//#DEBUG
+														//killerMoves.Clear();
 		timer = _timer;
 		historyTable = new int[2, 7, 64];
 		timeForTurn = Math.Min(timer.MillisecondsRemaining - 50, timer.MillisecondsRemaining / 30);
@@ -96,7 +97,7 @@ public class MyBot : IChessBot
 			if (timeToStop) break;
 			startime = timer.MillisecondsRemaining; //#DEBUG
 
-			bestEval = -negamax(board, d, 0, -10000000, 10000000, board.IsWhiteToMove ? 1 : -1);
+			bestEval = -negamax(d, 0, -10000000, 10000000);
 			Console.WriteLine($"info string best move at depth {d} was {overAllBestMove} with eval at {bestEval}");//#DEBUG
 			Console.WriteLine($"info string Time used for depth {d}: {startime - timer.MillisecondsRemaining} miliseconds");//#DEBUG
 			Console.WriteLine("info string -------node count------- " + counters[^1]);//#DEBUG
@@ -116,8 +117,11 @@ public class MyBot : IChessBot
 
 
 
-	//There are 3 tokens to save here, using fewer variables, and negating eval in each loop
-	int evaluation(Board board)
+	/// <summary>
+	/// Assumes global board variable exists, and evaluates that
+	/// </summary>
+	/// <returns></returns>
+	int evaluation()
 	{
 		int middlegame = 0, endgame = 0, gamephase = 0, sideToMove = 2;
 		for (; --sideToMove >= 0;)
@@ -140,7 +144,8 @@ public class MyBot : IChessBot
 		return (middlegame * gamephase + endgame * (24 - gamephase)) / 24;
 	}
 
-
+	//Global board saves tokens.
+	Board board;
 	/// <summary>
 	/// 
 	/// </summary>
@@ -152,7 +157,7 @@ public class MyBot : IChessBot
 	/// <param name="color"></param>
 	/// <param name="numExtensions"></param>
 	/// <returns></returns>
-	int negamax(Board board, sbyte depth, int ply, int alpha, int beta, int color)
+	int negamax(sbyte depth, int ply, int alpha, int beta)
 	{
 		depth = Math.Max(depth, (sbyte)0);
 		//start searching
@@ -182,18 +187,20 @@ public class MyBot : IChessBot
 		bool isQSearch = depth <= 0;
 		if (isQSearch)
 		{
-			max = color * evaluation(board); 
+			max = evaluation() * (board.IsWhiteToMove ? 1 : -1);
 
 			if (max >= beta)
 				return max;
 			alpha = Math.Max(alpha, max);
 		}
+		//local search function to save tokens.
+		int search(int reductions, int betas) => -negamax((sbyte)(depth - 1 - reductions), ply + 1, -betas, -alpha);
 
 		// Null move pruning - is perhaps best with more time on the clock?
 		if (!isPV && !isInCheck && depth > R)
 		{
 			board.TrySkipTurn();
-			int score = -negamax(board, (sbyte)(depth - R - 1), ply + 1, -beta, -alpha, -color);
+			int score = search(R, beta);
 			board.UndoSkipTurn();
 			if (score >= beta)
 				return score;
@@ -251,7 +258,6 @@ public class MyBot : IChessBot
 				return 0;
 
 			board.MakeMove(move);
-			int search(int reductions, int betas) => -negamax(board, (sbyte)(depth - 1 - reductions), ply + 1, -betas, -alpha, -color);
 			if (isQSearch)
 			{
 				eval = search(depth, beta);
@@ -261,7 +267,6 @@ public class MyBot : IChessBot
 				// LMR: reduce the depth of the search for moves beyond a certain move count threshold - Can save few tokens here with simpler reduction
 				int reduction = (int)((depth >= 4 && moveCount >= 4 && !move.IsCapture && !move.IsPromotion && !isInCheck && !isPV) ? 1 + Math.Log2(depth) * Math.Log2(moveCount) / 2 : 0);
 				//reduction = isPV && reduction > 0 ? 1 : 0;
-				//local search function to save tokens.
 
 				if (moveCount == 1)
 					eval = search(reduction, beta);
@@ -299,11 +304,11 @@ public class MyBot : IChessBot
 
 		// Transposition table store
 		entryCount++; //#DEBUG
-					  // Transposition table insertion
+						// Transposition table insertion
 		entry = new(
 			zobristHash,
 			bestFoundMove == default ? entry.Item2 : bestFoundMove,
-			max,	
+			max,
 			depth,
 			(byte)(max >= beta ? 3 : max <= oldAlpha ? 2 : 1));
 
